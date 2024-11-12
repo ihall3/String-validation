e9dfd85d5721449aaaca80b2856af1007f7c49f0
ihall3 <Ianhall0319@gmail.com>
2024-11-12 12:43:41 -0500

    Add project files.

---
+++ String validation/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Enter your role name (Administrator, Manager, or User)");
+string a;
+bool b = false;
+do
+{
+    a = Console.ReadLine();
+    if (a != null)
+    {
+        a = a.Trim();
+        a = a.ToLower();
+        if (a == "administrator" || a == "manager" || a == "user")
+        {
+            b = true;
+        }
+
+        else
+        {
+            Console.WriteLine($"The role name that you entered, \"{a}\" is not valid.");
+            Console.WriteLine("Enter your role name (Administrator, Manager, or User)");
+        }
+    }
+}
+while (b == false);
+Console.WriteLine($"Your input value ({a}) has been accepted.");
